0597e77b0f79070c84cc9ce1629c713f0687bba1
Katreque <renanverissimo@uol.com.br>
2017-06-18 20:11:50 -0300

    Beta released

--- libs/movimentoController.h
+++ libs/movimentoController.h
@@ -1,6 +1,8 @@
 #ifndef movimentoController
 #define movimentoController
 #include <GL/glut.h>
+#include<iostream>
+#include <time.h>
 
 void movimentaPlayer(GLdouble xc, GLdouble yc){
 	glPushMatrix();
@@ -10,17 +12,37 @@ void movimentaPlayer(GLdouble xc, GLdouble yc){
 	glPopMatrix();	
 }
 
-int tiroLinear() {
-	int r = (rand() % 100) - 48;
+unsigned long mix(unsigned long a, unsigned long b, unsigned long c){
+    a=a-b;  a=a-c;  a=a^(c >> 13);
+    b=b-c;  b=b-a;  b=b^(a << 8);
+    c=c-a;  c=c-b;  c=c^(b >> 13);
+    a=a-b;  a=a-c;  a=a^(c >> 12);
+    b=b-c;  b=b-a;  b=b^(a << 16);
+    c=c-a;  c=c-b;  c=c^(b >> 5);
+    a=a-b;  a=a-c;  a=a^(c >> 3);
+    b=b-c;  b=b-a;  b=b^(a << 10);
+    c=c-a;  c=c-b;  c=c^(b >> 15);
+    return c;
+}
+
+unsigned int criaSeed() {
+	unsigned long seed = mix(clock(), time(NULL), getpid());
+	return seed;
+}
+
+int pontoPartidaX() {
+	srand(criaSeed());
+	int r = (rand() % 98) - 48;
 	return r;
 }
 
-int tiroLinear1() {
-	return -2;
-}
-
-int tiroLinear2() {
-	return -4;
+int pontoPartidaY() {
+	srand(criaSeed());
+	int r = (rand() % 60);
+	if(r < 52){
+		r += 60 - r;
+	}
+	return r;
 }
 	
 #endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <cstdlib>
+#include <windows.h>
 #include <GL/glut.h>
+#include <GL/freeglut.h>
 #include <movimentoController.h>
 #include <bulletsController.h>
+
+#define TOTALTIROS 20
 
 struct PlayerOne {
 	GLdouble x;
@@ -14,12 +18,57 @@ struct Normalbullet {
 	GLdouble y;
 };
 
+int score = 0, finalScore;
+GLdouble velocidadeTiros = 55;
+bool fim = false;
+
+unsigned char final[] = "Game Over!";
+
 PlayerOne player1;
-Normalbullet normalbullet[10];
+Normalbullet normalbullet[TOTALTIROS];
 
 void inicializacao() {
+	PlaySound("C:\\Users\\rveri_000\\Desktop\\Programacao\\ibashou\\MainTheme.wav", NULL, SND_ASYNC|SND_FILENAME|SND_LOOP);
+	
 	player1.x = 0;
 	player1.y = -45;
+	
+	for(int i = 0; i < TOTALTIROS; i++){	
+		normalbullet[i].x = pontoPartidaX();
+		normalbullet[i].y = pontoPartidaY();
+	}
+}
+
+void tiroSaiuDoMapa(){
+	for(int i = 1; i < TOTALTIROS; i++){
+		if(normalbullet[i].x < -70 || normalbullet[i].x > 70 || normalbullet[i].y < -70|| normalbullet[i].y > 70){
+			normalbullet[i].x = pontoPartidaX();
+			normalbullet[i].y = pontoPartidaY();
+			
+		score++;
+		printf("%d\n", score);
+		
+			if(score > 20){
+				velocidadeTiros = 45;
+			}
+			
+			if(score > 50){
+				velocidadeTiros = 30;
+			}
+			
+			if(score > 100){
+				velocidadeTiros = 20;
+			}
+			
+			if(score > 250){
+				velocidadeTiros = 10;
+			}
+			
+			if(score > 400){
+				velocidadeTiros = 5;
+			}
+		}	
+	}
 }
 
 void myReshape(int w, int h){
@@ -39,12 +88,66 @@ void myDisplay(void){
      gluLookAt(0, 0, 50,
               0, 0, 0,
               0, 1, 0);
+              
+    //glRasterPos2i(40, 45);
+	//glColor3f(1, 1, 1);
+	//glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, score);
  	
     movimentaPlayer(player1.x, player1.y); 
-    for(int i = 0; i < 1; i++){
+    for(int i = 0; i < TOTALTIROS; i++){
     	normalBullet(normalbullet[i].x, normalbullet[i].y);			
 	}
-    
+	
+	if(score >= 0 && score < 8){
+		unsigned char fase [] = "Use as setas do teclado para se movimentar e tente não MORRER!";
+		glRasterPos2i(-47, 20);
+		glColor3f(1, 1, 0);
+		glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24, fase);
+	}
+	
+	if(score > 20 && score < 25){
+		unsigned char fase [] = "Fase 01 - Bronze 8";
+		glRasterPos2i(-13, 20);
+		glColor3f(1, 1, 0);
+		glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24, fase);
+	}
+	
+	if(score > 50 && score < 60){
+		unsigned char fase [] = "Fase 02 - KappaPride";
+		glRasterPos2i(-13, 20);
+		glColor3f(1, 1, 0);
+		glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24, fase);
+	}
+	
+	if(score > 100 && score < 110){
+		unsigned char fase [] = "Fase 03 - High elo Gameplay";
+		glRasterPos2i(-17, 20);
+		glColor3f(1, 1, 0);
+		glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24, fase);
+	}
+	
+	if(score > 250 && score < 270){
+		unsigned char fase [] = "Fase 04 - God do God";
+		glRasterPos2i(-13, 20);
+		glColor3f(1, 1, 0);
+		glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24, fase);
+	}
+	
+	if(score > 400){
+		unsigned char fase [] = "Final Boss - Katreque's mind";
+		glRasterPos2i(-20, 20);
+		glColor3f(1, 1, 0);
+		glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24, fase);
+	}
+	
+	if(fim) {
+		glRasterPos2i(-8, 0);
+		glColor3f(1, 1, 0);
+		glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24, final);
+		velocidadeTiros = -1;
+	}
+	
+	tiroSaiuDoMapa();
 	glutSwapBuffers();
 }
 
@@ -86,27 +189,43 @@ void controlesPlayer(int key, int x, int y){
 	glutPostRedisplay();
 }
 
-void calcTiro(){
-	for(int i = 0; i < 1; i+=3){
-		int r = tiroLinear();
-		
-		normalbullet[i].x = r;
-		normalbullet[i].y -= 1;
+void mov(){
+	for(int i = 1; i < TOTALTIROS; i+=2){
+		normalbullet[i].x += -0.3;	
+		normalbullet[i].y += -0.5;
+	}
+	
+	for(int i = 0; i < TOTALTIROS; i+=2){
+		normalbullet[i].x += 0.2;	
+		normalbullet[i].y += -0.5;
+	}
+	
+	for(int i = 0; i < TOTALTIROS; i+=9){	
+		normalbullet[i].y += -0.5;
 	}
 }
 
-void mov(){
-	for(int i = 0; i < 1; i+=3){	
-		normalbullet[i].x += -1;
+int bateu(){
+	for(int i = 0; i < TOTALTIROS; i++){
+		if( ((normalbullet[i].x >= player1.x - 2) && (normalbullet[i].x <= player1.x + 2)) &&
+			((normalbullet[i].y >= player1.y - 2) && (normalbullet[i].y <= player1.y + 2))		
+		){
+			finalScore = score;
+			//printf("%d", finalScore);
+			fim = true;
+			PlaySound("C:\\Users\\rveri_000\\Desktop\\Programacao\\ibashou\\GameOver2.wav", NULL, SND_ASYNC|SND_FILENAME);
+		}
 	}
 }
 
 void time(int id){
 	
 	mov();
+	bateu();
     glutPostRedisplay();
-    glutTimerFunc(60, time, 1);
+    glutTimerFunc(velocidadeTiros, time, 1);
 }
+
 
 int main(int argc, char** argv){
     glutInit(&argc, argv);
@@ -121,7 +240,6 @@ int main(int argc, char** argv){
     
     inicializacao();
 	glutSpecialFunc(controlesPlayer);
-	calcTiro();
 	glutTimerFunc(60, time, 0);
 	
     glutMainLoop();
